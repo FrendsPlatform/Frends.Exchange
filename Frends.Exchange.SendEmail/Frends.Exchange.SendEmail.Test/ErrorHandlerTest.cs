b12c299061f492f1cdd54bd99ff294d87d974bb8
MichalFrends1 <michal.nowakowski@frends.com>
2026-09-17 10:49:07 +0200

    fix error hanlder tests

--- Frends.Exchange.SendEmail/Frends.Exchange.SendEmail.Test/ErrorHandlerTest.cs
+++ Frends.Exchange.SendEmail/Frends.Exchange.SendEmail.Test/ErrorHandlerTest.cs
@@ -11,30 +11,40 @@ public class ErrorHandlerTest
     private const string CustomErrorMessage = "CustomErrorMessage";
     private static Connection _connection = new();
     private static Input _input = new();
-    private static Options _options = new();
 
     [TestMethod]
     public async Task Should_Throw_Error_When_ThrowErrorOnFailure_Is_True()
     {
+        var options = new Options
+        {
+            ThrowErrorOnFailure = true
+        };
         var ex = await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
-            await Exchange.SendEmail(_input, _connection, _options, default));
+            await Exchange.SendEmail(_input, _connection, options, default));
         Assert.IsNotNull(ex);
     }
 
     [TestMethod]
     public async Task Should_Return_Failed_Result_When_ThrowErrorOnFailure_Is_False()
     {
-        _options.ThrowErrorOnFailure = false;
-        var result = await Exchange.SendEmail(_input, _connection, _options, default);
+        var options = new Options
+        {
+            ThrowErrorOnFailure = false
+        };
+        var result = await Exchange.SendEmail(_input, _connection, options, default);
         Assert.IsFalse(result.Success);
     }
 
     [TestMethod]
     public async Task Should_Use_Custom_ErrorMessageOnFailure()
     {
-        _options.ErrorMessageOnFailure = CustomErrorMessage;
+
+        var options = new Options
+        {
+            ErrorMessageOnFailure = CustomErrorMessage
+        };
         var ex = await Assert.ThrowsExceptionAsync<Exception>(async () =>
-            await Exchange.SendEmail(_input, _connection, _options, default));
+            await Exchange.SendEmail(_input, _connection, options, default));
         Assert.IsNotNull(ex);
         StringAssert.Contains(ex.Message, CustomErrorMessage);
     }
